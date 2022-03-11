4a885061ce2cc873b60f3f4b941f230e5247929a
George Spanos <gespa11019@gmail.com>
2022-03-11 02:41:44 +0200

    Room setup

--- apps/chat/chat-api/src/app/rooms/rooms.controller.ts
+++ apps/chat/chat-api/src/app/rooms/rooms.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotImplementedException, Query } from '@nestjs/common';
+import { Room } from '@oursocial/domain';
 
 @Controller('rooms')
-export class RoomsController {}
+export class RoomsController {
+  @Get()
+  async roomById(@Query() roomId: string): Promise<Room> {
+    throw new NotImplementedException();
+  }
+  async roomsByUserId(@Query() userId: string) {
+    throw new NotImplementedException();
+  }
+  async addUserToRoom(@Query() userId: string, @Query() roomId: string) {
+    throw new NotImplementedException();
+  }
+}
