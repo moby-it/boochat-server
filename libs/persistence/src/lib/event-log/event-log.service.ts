fbda11ca3debb61037906aef4b27cadbfb20f343
George Spanos <gespa11019@gmail.com>
2022-03-27 21:31:01 +0300

    Last visit is one per user per room

--- libs/persistence/src/lib/event-log/event-log.service.ts
+++ libs/persistence/src/lib/event-log/event-log.service.ts
@@ -8,19 +8,10 @@ export class EventLogPersistenceService {
   constructor(@InjectModel(EventLog.name) private model: Model<EventLogDocument>) { }
   async logVisit(roomId: string, userId: string, timestamp: Date): Promise<void> {
     if (!roomId || !userId) throw new Error('Cannot log visit with no roomId or userId');
-    const eventLog = new this.model({
-      type: EventsEnum.USER_DISCONNECTED_FROM_ROOM,
-      payload: {
-        room: new Types.ObjectId(roomId),
-      },
-      user: new Types.ObjectId(userId),
-      timestamp
-    });
-    await eventLog.save();
-
-  }
-  async getLastVisitsByUserId(userId: string): Promise<EventLogDocument[]> {
-    if (!userId) throw new Error('Cannot get last visit with no userId');
-    return await this.model.find({ user: new Types.ObjectId(userId), type: EventsEnum.USER_DISCONNECTED_FROM_ROOM }).exec();
+    await this.model.updateOne(
+      { room: new Types.ObjectId(roomId), user: new Types.ObjectId(userId), type: EventsEnum.USER_DISCONNECTED_FROM_ROOM },
+      { timestamp },
+      { upsert: true }
+    );
   }
 }
