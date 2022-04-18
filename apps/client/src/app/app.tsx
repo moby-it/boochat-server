136f92b3126bab5b59b64af2d7c2bf6009e89e72
George Spanos <gespa11019@gmail.com>
2022-04-18 18:10:04 +0300

    login init

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,27 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import ContentLoader from 'react-content-loader';
 import { io, Socket } from 'socket.io-client';
 import { environment } from '../environments/environment';
 import { ActiveRoomContainer } from './active-room/active-room-container';
 import './app.module.css';
+import Login from './login/login';
 import { MeetupsContainer } from './meetups/meetups-container';
 import { RoomListContainer } from './rooms/room-list-container';
 import { Sidenav } from './sidenav/Sidenav';
 const token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjhmYWU2ZDQzLWEyNDgtNGIwNS1hZDdhLTBkZTM3N2VjYTA4MiIsImdvb2dsZUlkIjoiZ2V5dWlzZWZ5dWlzZWZpb3JnaW8iLCJuYW1lIjoieXVpc2VmIiwiaW1hZ2VVcmwiOiJodHRwczovL2kxLnNuZGNkbi5jb20vYXJ0d29ya3MtWTJIMGkwaXY2ekdMY0UwNC1rdTcwSkEtdDUwMHg1MDAuanBnIiwiaWF0IjoxNjUwMjg4OTg4LCJleHAiOjE2NTAzMDMzODh9.YSpFTiw2Cu6EoEMrxVJzspaQJ1eqxLSEem7aZqhJ6yg';
 export function App() {
+  const [isLoggedIn, logIn] = useState(false);
+  const [connected, connect] = useState(false);
+
   useEffect(() => {
-    // const commandSocket = io(environment.commandApiUrl, {
-    //   extraHeaders: {
-    //     authorization: ''
-    //   }
-    // }).connect();
-    io(environment.queryApiUrl + `?token=${token}`, {
+    commandSocket = io(environment.commandApiUrl, {
+      extraHeaders: {
+        authorization: ''
+      }
+    }).connect();
+    querySocket = io(environment.queryApiUrl + `?token=${token}`, {
       transports: ['websocket']
     }).connect();
     // querySocket.on('ALL_USERS', (users) => {
     //   console.log(users);
     // });
   }, []);
+  if (!isLoggedIn) return <Login />;
+  if (!connected) return <ContentLoader />;
   return (
     <div className="app-shell">
       <Sidenav />
---
+++ apps/client/src/app/login/login.tsx
@@ -0,0 +1,4 @@
+export function Login() {
+  return <h2>Please Login</h2>;
+}
+export default Login;
