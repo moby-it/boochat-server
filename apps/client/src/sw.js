e26ecc8a82f1bf1ca91ef87b8c74452a3e03f639
George Spanos <gespa11019@gmail.com>
2022-04-23 19:02:04 +0300

    pwa first version

--- apps/client/src/sw.js
+++ apps/client/src/sw.js
@@ -1,7 +1,15 @@
 /* eslint-disable no-restricted-globals */
-// eslint-disable-next-line @typescript-eslint/no-unused-expressions
-self.__WB_MANIFEST;
-
-self.addEventListener('fetch', (data) => {
-  console.log(data.request.url);
+/* eslint-disable no-undef */
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+self.addEventListener('install', () => {
+  console.log('clearing cache...');
+  caches.keys().then((cacheNames) => {
+    cacheNames.forEach((cacheName) => {
+      caches.delete(cacheName);
+    });
+  });
 });
+
+const { precacheAndRoute } = workbox.precaching;
+
+precacheAndRoute(self.__WB_MANIFEST);
