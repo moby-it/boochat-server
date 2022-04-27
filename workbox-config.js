530b93f018c45a66c30d8772a1eb28a0da0f682a
George Spanos <gespa11019@gmail.com>
2022-04-27 11:40:02 +0300

    remove pwa fixes

--- workbox-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globDirectory: 'dist/apps/client',
-  globPatterns: ['**/*.{js,css,ico,html,json,png}'],
-  swDest: 'dist/apps/client/sw.js',
-  swSrc: 'apps/client/src/sw.js'
-};
