d71757880e161c54a0ad20a5c9fe1cabacec433f
George Spanos <gespa11019@gmail.com>
2022-05-14 15:43:11 +0300

    import notification module

--- apps/query-api/src/app/users/users.module.ts
+++ apps/query-api/src/app/users/users.module.ts
@@ -1,4 +1,4 @@
-import { ActiveUsersModule, AuthModule } from '@boochat/application';
+import { ActiveUsersModule, AuthModule, NotificationModule } from '@boochat/application';
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { UserEventsController } from './user-events.controller';
@@ -6,7 +6,7 @@ import { UsersGateway } from './users.gateway';
 
 @Module({
   controllers: [UserEventsController],
-  imports: [CqrsModule, AuthModule, ActiveUsersModule],
+  imports: [CqrsModule, AuthModule, ActiveUsersModule, NotificationModule],
   providers: [UsersGateway]
 })
 export class UserModule {}
